845338e361ee0c4b988f42bba267c66f4c46c848
Mukesh Kumar <mukeshrai5977@gmail.com>
2024-10-07 00:31:08 +0530

    fix jwt error in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,6 +3,7 @@ import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js';
 import { User } from "../models/user.model.js";
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshToken = async(userId) =>{
   try {
